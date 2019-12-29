8494a00d01e1658938cd31a215b9bfeb327b75df
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2019-12-28 20:02:43 -0500

    define your connection string for mongodb db

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-// );
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//   console.log("MongoDB database connection established successfully");
-// })
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
 // const exercisesRouter = require('./routes/exercises');
 // const usersRouter = require('./routes/users');
